01379db89fc8fd1ef8e2c72bb9e2177e29514586
Julija777 <s.julija@icloud.com>
2021-12-22 16:57:27 +0000

    Instruction for the Kata written

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 //👉 Write your kata here!
 
+//MinMax kata getting true or false
+
+// Write a function which will get the minimum and the maximum
+// Number from the array and will sum up those two numbers.
+// The outcome of the function should return TRUE for even number
+// and FALSE for odd number .
+// If an array is empty, null or None then your function should return 0
+// If only one element in the array then your function should return 0.
+// If input in the array not a number your function should return “Use numbers”.
+// For example:
+//  [4, 5, 3, 2, 1] return True
+
 //👉 Write the function your CodeWarriors will start with below here:
 function sumMinMaxArr(arr) {
   // Your code
